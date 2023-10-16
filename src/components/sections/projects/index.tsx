8a33e665f2b2ee614ca05307b62459f0c4f29098
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-10-16 18:25:50 -0300

    feat(projects): display projects content

--- src/components/sections/projects/index.tsx
+++ src/components/sections/projects/index.tsx
@@ -3,54 +3,52 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from './Projects.module.css';
 
+import DogsImg from '../../../../public/img/dogs_site.jpg';
+import WizerImg from '../../../../public/img/wizer_site.jpg';
+import PlayIcon from '../../../../public/play_icon.svg';
+import DisabledPlayIcon from '../../../../public/play_disabled_icon.svg';
+
 
 const skills = ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js'];
 
 const projects = [
   {
-    title: 'Projeto 1',
+    title: 'Dogs',
     technologies: 'React, Node.js',
-    githubLink: 'https://github.com/seuprojeto1',
-    siteLink: '',
-    imageUrl: '/images/project1.jpg',
-    descricao: 'Descrição do Projeto 1',
-    skills: ['React', 'Node.js'],
+    githubLink: 'https://github.com/Lucas-A-Araujo/Dogs',
+    siteLink: 'https://lucas-a-araujo.github.io/Dogs/',
+    imageUrl: DogsImg,
+    descricao: 'Uma rede social feita para donos de cachorros'
+    + ' que desejam mostrar o seu pet para o mundo e ver os dos outros donos.',
+    skills: ['React', 'Javascript'],
   },
   {
-    title: 'Projeto 2',
+    title: 'Wizer',
     technologies: 'React, HTML, CSS',
-    githubLink: 'https://github.com/seuprojeto2',
-    siteLink: 'https://seusite2.com',
-    imageUrl: '/images/project2.jpg',
-    descricao: 'Descrição do Projeto 1',
-    skills: ['React', 'HTML', 'CSS'],
+    githubLink: '',
+    siteLink: '',
+    imageUrl: WizerImg,
+    descricao: 'Uma plataforma de cursos de tecnologia com diversas trilhas'
+    +' e cursos que permite você acompanhar o seu progresso de maneira eficiente.',
+    skills: ['React', 'Next', 'Tailwind'],
   },
-  // Adicione mais projetos aqui
 ];
 
 export function ProjectsSection() {
   const [filter, setFilter] = useState(['all']);
 
-  const handleFilter = (skill: string[]) => {
-    // Implemente a lógica de filtro aqui, se necessário
-  };
-
   const handleAddFilter = async (skill: string) => {
-    // Implemente a lógica de filtro aqui, se necessário
     if(skill === 'all') {
       setFilter(['all']);
-      console.log(filter);
     }
     else if(filter.includes(skill)) {
-      const newFilter = filter.filter((item) => (item !== skill && item !== 'all'));
+      let newFilter = filter.filter((item) => (item !== skill && item !== 'all'));
+      if(newFilter.length === 0) newFilter = ['all'];
       setFilter(newFilter);
-      console.log(filter);
     }
     else {
       const newFilter = filter.filter((item) => item !== 'all');
       setFilter([...newFilter, skill]);
-      console.log(filter);
-
     }
   };
 
@@ -63,25 +61,44 @@ export function ProjectsSection() {
 
 
   return (
-    <div>
       <section className={styles.projects} id="portfolio">
       <div className={styles.filtro}>
-        <button onClick={() => handleAddFilter('all')}>Todos</button>
-        <button onClick={() => handleAddFilter('React')}>React</button>
-        <button onClick={() => handleAddFilter('Node.js')}>Node.js</button>
-        <button onClick={() => handleAddFilter('CSS')}>CSS</button>
-        {/* Adicione mais botões para outras tecnologias, se necessário */}
+        <button className={filter.includes('all') ? styles.active : ''} onClick={() => handleAddFilter('all')}>Todos</button>
+        <button className={filter.includes('React') ? styles.active : ''} onClick={() => handleAddFilter('React')}>React</button>
+        <button className={filter.includes('Node.js') ? styles.active : ''} onClick={() => handleAddFilter('Node.js')}>Node.js</button>
+        <button className={filter.includes('CSS') ? styles.active : ''} onClick={() => handleAddFilter('CSS')}>CSS</button>
       </div>
       <div className={styles.listaProjetos}>
         {projetosFiltrados.map(project => (
           <div key={project.title} className={styles.projeto}>
+            <div className={styles.projectHeader}>
             <h2>{project.title}</h2>
+            {project.siteLink ? (
+              <div className={styles.buttonContainer}>
+              <Image alt="Github" src={PlayIcon}
+              width="32" height="32" />
+                <a
+                  href={project.siteLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.siteLinkButton}
+                >
+                  Abrir Site
+                </a>
+                </div>
+              ) : (
+                <div className={styles.buttonContainer}>
+                  <Image alt="Github" src={DisabledPlayIcon}
+              width="32" height="32" />
+                  <p className={styles.comingSoonText}>Em breve</p>
+                </div>
+              )}
+              </div>
+            <Image alt={`Imagem ilustrando o site ${project.title}`} src={project.imageUrl} width="512" />
             <p>{project.descricao}</p>
           </div>
         ))}
       </div>
-        {/* <ProjectFilter skills={skills} projects={projects} onFilter={handleFilter} /> */}
       </section>
-    </div>
   );
 }
